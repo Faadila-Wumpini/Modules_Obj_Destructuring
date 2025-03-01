719918e283ca7e4091eaba617c3a3b45cdb79f5d
Faadila Wumpini Iddrisu <faadilawumpini@gmail.com>
2025-03-01 15:00:52 -0800

    Add files via upload

---
+++ Object destructuring and modules/modules_export.js
@@ -0,0 +1,7 @@
+export const sqrt = Math.sqrt;
+export function square(x) {
+    return x*x;
+}
+export function dialog(x, y){
+    return sqrt (square(x) + square(y));
+}
---
+++ Object destructuring and modules/modules_import.js
@@ -0,0 +1,3 @@
+import * as modules_import from modules_import;
+console.log(modules_import.square(25));
+console.log(modules_import.diag(5,3));
